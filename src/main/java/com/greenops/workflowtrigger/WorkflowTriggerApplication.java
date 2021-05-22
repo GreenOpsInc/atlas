900c1dbc0ac3e814c00225ea02fe03a3228dbaee
mihirpandya-greenops <mihir@greenops.io>
2021-05-21 18:43:44 -0700

    Project structure setup

---
+++ src/main/java/com/greenops/workflowtrigger/WorkflowTriggerApplication.java
@@ -0,0 +1,13 @@
+package com.greenops.workflowtrigger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowTriggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowTriggerApplication.class, args);
+	}
+
+}
