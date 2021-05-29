a206943f0454eb21f8f63a3b59a07101f411e44f
mihirpandya-greenops <mihir@greenops.io>
2021-05-28 18:09:23 -0700

    setting up project

---
+++ src/main/java/com/greenops/pipelinereposerver/PipelinereposerverApplication.java
@@ -0,0 +1,13 @@
+package com.greenops.pipelinereposerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinereposerverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinereposerverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenops/pipelinereposerver/PipelinereposerverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenops.pipelinereposerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinereposerverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
