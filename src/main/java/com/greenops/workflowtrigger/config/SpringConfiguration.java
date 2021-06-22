7601b67866336f22ec8a7ba78df77afb07838c78
mihirpandya-greenops <84353489+mihirpandya-greenops@users.noreply.github.com>
2021-06-22 14:24:44 -0700

    Adding in Workflow Orchestrator config into the Workflow Trigger (#30)

--- src/main/java/com/greenops/workflowtrigger/config/SpringConfiguration.java
+++ src/main/java/com/greenops/workflowtrigger/config/SpringConfiguration.java
@@ -40,15 +40,6 @@ public class SpringConfiguration {
     }
 
     @Bean
-    public ProducerFactory<String, String> producerFactory(@Value("${spring.kafka.producer.bootstrap-servers}") String kafkaBootstrapAddress) {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapAddress);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
--- src/test/test_config/deploy_test_config.sh
+++ src/test/test_config/deploy_test_config.sh
@@ -3,5 +3,7 @@ cd ../../../
 ./gradlew jibDockerBuild --image=atlasworkflowtrigger
 cd ../PipelineRepoServer/
 ./gradlew jibDockerBuild --image=atlasreposerver
+cd ../WorkflowOrchestrator/
+./gradlew jibDockerBuild --image=atlasworkfloworchestrator
 cd ../WorkflowTrigger/src/test/test_config
 docker compose up -d
