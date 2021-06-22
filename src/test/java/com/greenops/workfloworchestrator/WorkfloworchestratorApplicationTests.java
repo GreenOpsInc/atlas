d2d638f92a7e573d1500a33719768c92e99a40c6
mihirpandya-greenops <mihir@greenops.io>
2021-06-22 01:13:33 -0700

    Setting up repo

---
+++ src/test/java/com/greenops/workfloworchestrator/WorkfloworchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenops.workfloworchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkfloworchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
