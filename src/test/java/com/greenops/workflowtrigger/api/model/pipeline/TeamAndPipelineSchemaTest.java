7ea909cece079e50104f40aa8ac45ae416824900
chiru-koiloth <84358616+chiru-koiloth@users.noreply.github.com>
2021-07-19 19:28:26 -0700

    Team and Pipeline Schema usage tests (#33)
    * Team and Pipeline Schema tests
    
    * Added extra assertion to test setPathToRoot
    
    * Testing setting git cred functionality of git repo schema
    
    Co-authored-by: Gaurav <gaurav@greenops.io>
    Co-authored-by: gsharma30 <84997560+gsharma30@users.noreply.github.com>

--- src/test/java/com/greenops/workflowtrigger/api/model/pipeline/TeamAndPipelineSchemaTest.java
+++ src/test/java/com/greenops/workflowtrigger/api/model/pipeline/TeamAndPipelineSchemaTest.java
@@ -1,9 +1,9 @@
 package com.greenops.workflowtrigger.api.model.pipeline;
 
-import com.greenops.workflowtrigger.api.model.git.GitCredOpen;
-import com.greenops.workflowtrigger.api.model.git.GitRepoSchema;
+import com.greenops.workflowtrigger.api.model.git.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -25,11 +25,48 @@ public class TeamAndPipelineSchemaTest {
         teamSchema.addPipeline("pipeline2", gitRepoSchema);
         assertTrue(teamSchema.getPipelineNames().contains("pipeline1"));
         assertTrue(teamSchema.getPipelineNames().contains("pipeline2"));
-        assertEquals(teamSchema.getPipelineNames().size(),2);
+        assertEquals(teamSchema.getPipelineNames().size(), 2);
         teamSchema.removePipeline(pipeline1.getPipelineName());
-        assertEquals(teamSchema.getPipelineNames().size(),1);
+        assertEquals(teamSchema.getPipelineNames().size(), 1);
         assertTrue(teamSchema.getPipelineNames().contains("pipeline2"));
         assertEquals("team1", teamSchema.getTeamName());
         assertEquals(TeamSchema.ROOT_TEAM, teamSchema.getParentTeam());
+    }
+
+    @Test
+    void testPipelineSchemaCanSetAndGetPipeline() {
+        var gitCredMachineUser = new GitCredMachineUser("root", "admin");
+        var gitRepoSchema = new GitRepoSchema("https://github.com/argoproj/argocd-example-apps.git", "guestbook/", gitCredMachineUser);
+        var pipelineSchema = new PipelineSchemaImpl("test_pipeline", gitRepoSchema);
+        assertEquals("test_pipeline", pipelineSchema.getPipelineName());
+        assertEquals("https://github.com/argoproj/argocd-example-apps.git", pipelineSchema.getGitRepoSchema().getGitRepo());
+        assertEquals("guestbook/", pipelineSchema.getGitRepoSchema().getPathToRoot());
+        pipelineSchema.setPipelineName("changed_test_pipeline_name");
+        assertEquals("changed_test_pipeline_name", pipelineSchema.getPipelineName());
+
+        gitRepoSchema = new GitRepoSchema("https://github.com/argoproj/argo-workflows.git", "src/", new GitCredOpen());
+        pipelineSchema.setGitRepoSchema(gitRepoSchema);
+        assertEquals("https://github.com/argoproj/argo-workflows.git", pipelineSchema.getGitRepoSchema().getGitRepo());
+        assertEquals("src/", pipelineSchema.getGitRepoSchema().getPathToRoot());
+        assert (pipelineSchema.getGitRepoSchema().getGitCred() instanceof GitCredOpen);
+
+        gitRepoSchema.setGitCred(new GitCredToken("test_token"));
+        assert (pipelineSchema.getGitRepoSchema().getGitCred() instanceof GitCredToken);
+
+        gitRepoSchema.setGitCred(new GitCredMachineUser("test_user", "test_pwd"));
+        assert (pipelineSchema.getGitRepoSchema().getGitCred() instanceof GitCredMachineUser);
+    }
+
+    @Test
+    void testGitRepoSchema() {
+        var gitRepoSchema = new GitRepoSchema("https://github.com/argoproj/argo-workflows.git", "src/", new GitCredOpen());
+        assertEquals("https://github.com/argoproj/argo-workflows.git", gitRepoSchema.getGitRepo());
+        assertEquals("src/", gitRepoSchema.getPathToRoot());
+
+        gitRepoSchema.setGitRepo("https://github.com/argoproj/argocd-example.git");
+        assertEquals("https://github.com/argoproj/argocd-example.git", gitRepoSchema.getGitRepo());
+
+        gitRepoSchema.setPathToRoot("test/");
+        assertEquals("test/", gitRepoSchema.getPathToRoot());
     }
 }
