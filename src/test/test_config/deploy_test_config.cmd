f8c6d7070ba7af9ef909f03bae6bae23e062e921
mihirpandya-greenops <mihir@greenops.io>
2021-08-04 23:16:35 -0700

    Move workflowtrigger files to directory

--- src/test/test_config/deploy_test_config.cmd
+++
@@ -1,7 +0,0 @@
-docker-compose down
-cd ../../../
-call ./gradlew jibDockerBuild --image=atlasworkflowtrigger
-cd ../PipelineRepoServer/
-call ./gradlew jibDockerBuild --image=atlasreposerver
-cd ../WorkflowTrigger/src/test/test_config
-docker-compose up -d
