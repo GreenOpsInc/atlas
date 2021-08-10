69786a8599d719979b8443b5b7948c19b419971a
mihirpandya-greenops <mihir@greenops.io>
2021-08-10 16:21:15 -0700

    Adding stuff

---
+++ workflowtrigger/src/main/java/com/greenops/workflowtrigger/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.greenops.workflowtrigger.security;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//    }
+//}
