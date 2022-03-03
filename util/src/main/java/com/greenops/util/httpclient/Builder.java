bc5490b6b481cb5219cfd97f3fe892047ba1fad3
Hakky54 <hakangoudberg@hotmail.com>
2022-03-03 10:08:54 +0100

    Refactor ssl configuration

--- util/src/main/java/com/greenops/util/httpclient/Builder.java
+++ util/src/main/java/com/greenops/util/httpclient/Builder.java
@@ -8,10 +8,11 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Component;
 
-import javax.net.ssl.*;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509ExtendedKeyManager;
+import javax.net.ssl.X509ExtendedTrustManager;
 import java.io.File;
 import java.nio.file.Paths;
-import java.security.SecureRandom;
 
 @Slf4j
 @Component
@@ -49,29 +50,16 @@ public class Builder {
         return this;
     }
 
-    public HttpClient build() throws Exception {
+    public HttpClient build() {
         if (this.sslCon == null) {
-            SSLContext context = SSLContext.getInstance("TLS");
-            context.init(null, trustAllCerts, new SecureRandom());
-            this.builder.setSSLHostnameVerifier((s1, s2) -> true);
-            return this.builder.setSSLContext(context).build();
+            SSLFactory sslFactory = SSLFactory.builder()
+                    .withUnsafeTrustMaterial()
+                    .withUnsafeHostnameVerifier()
+                    .build();
+            this.builder.setSSLHostnameVerifier(sslFactory.getHostnameVerifier());
+            return this.builder.setSSLContext(sslFactory.getSslContext()).build();
         }
         return this.builder.setSSLContext(this.sslCon).build();
     }
 
-    private static final TrustManager[] trustAllCerts = new TrustManager[]{
-            new X509TrustManager() {
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return null;
-                }
-
-                public void checkClientTrusted(
-                        java.security.cert.X509Certificate[] certs, String authType) {
-                }
-
-                public void checkServerTrusted(
-                        java.security.cert.X509Certificate[] certs, String authType) {
-                }
-            }
-    };
 }
