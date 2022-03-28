ad373be2061c3a659bf9e917841c790fafb0bc07
Vlad Morzhanov <vlad.morzhanov@gmail.com>
2022-03-28 15:02:24 +0300

    fix code generation and add crd yaml file for pipeline

--- pkg/apis/pipeline/v1/types.go
+++ pkg/apis/pipeline/v1/types.go
@@ -1,14 +1,20 @@
 package v1
 
 import (
+	"encoding/json"
+	"strings"
+
 	"github.com/greenopsinc/util/auditlog"
-	"github.com/greenopsinc/util/git"
+	"github.com/greenopsinc/util/serializerutil"
 	"gitlab.com/c0b/go-ordered-json"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
 )
 
-// TODO: generate yaml file
+const (
+	hiddenInfo         string = "Hidden cred info"
+	secureGitUrlPrefix string = "https://"
+)
 
 // Pipeline is a definition of Pipeline resource.
 // +genclient
@@ -25,13 +31,12 @@ type Pipeline struct {
 
 // PipelineSpec represents desired pipeline state
 type PipelineSpec struct {
-	PipelineName string `json:"pipelineName"`
-	// TODO: move schema to this file
-	GitRepoSchema git.GitRepoSchema `json:"gitRepoSchema"`
+	PipelineName  string        `json:"pipelineName"`
+	GitRepoSchema GitRepoSchema `json:"gitRepoSchema"`
 }
 
 func UnmarshallPipelineSchema(m map[string]interface{}) PipelineSpec {
-	gitRepoSchema := git.UnmarshallGitRepoSchema(m["gitRepoSchema"].(map[string]interface{}))
+	gitRepoSchema := UnmarshallGitRepoSchema(m["gitRepoSchema"].(map[string]interface{}))
 	return PipelineSpec{
 		PipelineName:  m["pipelineName"].(string),
 		GitRepoSchema: gitRepoSchema,
@@ -42,7 +47,7 @@ func MarshalPipelineSchema(schema PipelineSpec) *ordered.OrderedMap {
 	mapObj := ordered.NewOrderedMap()
 	mapObj.Set("pipelineName", schema.PipelineName)
 
-	repo := git.MarshalGitRepoSchema(schema.GitRepoSchema)
+	repo := MarshalGitRepoSchema(schema.GitRepoSchema)
 	mapObj.Set("gitRepoSchema", repo)
 
 	return mapObj
@@ -140,9 +145,218 @@ type PipelineWatchEvent struct {
 }
 
 // PipelineList is list of Pipeline resources
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type PipelineList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
 	Items           []Pipeline `json:"items" protobuf:"bytes,2,rep,name=items"`
+}
+
+type GitRepoSchemaInfo struct {
+	GitRepoUrl string `json:"gitRepoUrl"`
+	PathToRoot string `json:"pathToRoot"`
+}
+
+func (g *GitRepoSchemaInfo) GetGitRepo() string {
+	return g.GitRepoUrl
+}
+
+func (g *GitRepoSchemaInfo) GetPathToRoot() string {
+	return g.PathToRoot
+}
+
+type GitRepoSchema struct {
+	GitRepo    string  `json:"gitRepo"`
+	PathToRoot string  `json:"pathToRoot"`
+	GitCred    GitCred `json:"gitCred"`
+}
+
+func New(gitRepo string, pathToRoot string, gitCred GitCred) GitRepoSchema {
+	return GitRepoSchema{
+		GitRepo:    gitRepo,
+		PathToRoot: pathToRoot,
+		GitCred:    gitCred,
+	}
+}
+
+func (g *GitRepoSchema) SetGitRepo(gitRepo string) {
+	g.GitRepo = gitRepo
+}
+
+func (g *GitRepoSchema) SetPathToRoot(pathToRoot string) {
+	g.PathToRoot = pathToRoot
+}
+
+func (g *GitRepoSchema) SetGitCred(gitCred GitCred) {
+	g.GitCred = gitCred
+}
+
+func (g *GitRepoSchema) GetGitRepo() string {
+	return g.GitRepo
+}
+
+func (g *GitRepoSchema) GetPathToRoot() string {
+	return g.PathToRoot
+}
+
+func (g *GitRepoSchema) GetGitCred() GitCred {
+	return g.GitCred
+}
+
+func (g *GitRepoSchema) ContentsEqual(schema GitRepoSchema) bool {
+	return g.GetGitRepo() == schema.GetGitRepo() && g.GetPathToRoot() == schema.GetPathToRoot()
+}
+
+func UnmarshallGitRepoSchema(m map[string]interface{}) GitRepoSchema {
+	gitCred := UnmarshallGitCred(m["gitCred"].(map[string]interface{}))
+	return GitRepoSchema{
+		GitRepo:    m["gitRepo"].(string),
+		PathToRoot: m["pathToRoot"].(string),
+		GitCred:    gitCred,
+	}
+}
+
+func UnmarshallGitRepoSchemaString(str string) GitRepoSchema {
+	var m map[string]interface{}
+	_ = json.Unmarshal([]byte(str), &m)
+	return UnmarshallGitRepoSchema(m)
+}
+
+func UnmarshallGitRepoSchemaInfo(m map[string]interface{}) GitRepoSchemaInfo {
+	return GitRepoSchemaInfo{
+		GitRepoUrl: m["gitRepoUrl"].(string),
+		PathToRoot: m["pathToRoot"].(string),
+	}
+}
+
+func UnmarshallGitRepoSchemaInfoString(str string) GitRepoSchemaInfo {
+	var m map[string]interface{}
+	_ = json.Unmarshal([]byte(str), &m)
+	return UnmarshallGitRepoSchemaInfo(m)
+}
+
+func MarshalGitRepoSchema(schema GitRepoSchema) *ordered.OrderedMap {
+	mapObj := ordered.NewOrderedMap()
+	mapObj.Set("gitRepo", schema.GitRepo)
+	mapObj.Set("pathToRoot", schema.PathToRoot)
+
+	cred := MarshalGitCred(schema.GetGitCred())
+	mapObj.Set("gitCred", cred)
+
+	return mapObj
+}
+
+type GitCred interface {
+	Hide()
+	DeepCopyGitCred() GitCred
+}
+
+type GitCredAccessible interface {
+	Hide()
+	ConvertGitCredToString(gitRepoLink string) string
+}
+
+// GitCredOpen Setting up GitCredOpen
+type GitCredOpen struct{}
+
+func (g *GitCredOpen) Hide() {}
+
+func (g *GitCredOpen) ConvertGitCredToString(gitRepoLink string) string {
+	return gitRepoLink
+}
+
+func (g *GitCredOpen) DeepCopyGitCred() GitCred {
+	return g.DeepCopy()
+}
+
+// GitCredMachineUser Setting up GitCredMachineUser
+type GitCredMachineUser struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (g *GitCredMachineUser) Hide() {
+	g.Username = hiddenInfo
+	g.Password = hiddenInfo
+}
+
+func (g *GitCredMachineUser) ConvertGitCredToString(gitRepoLink string) string {
+	splitUrl := strings.Split(gitRepoLink, secureGitUrlPrefix)
+	splitUrl[1] = g.Username + ":" + g.Password + "@" + splitUrl[1]
+	return secureGitUrlPrefix + splitUrl[1]
+}
+
+func (g *GitCredMachineUser) DeepCopyGitCred() GitCred {
+	return g.DeepCopy()
+}
+
+// GitCredToken Setting up GitCredToken
+type GitCredToken struct {
+	Token string `json:"token"`
+}
+
+func (g *GitCredToken) Hide() {
+	g.Token = hiddenInfo
+}
+
+func (g *GitCredToken) ConvertGitCredToString(gitRepoLink string) string {
+	splitUrl := strings.Split(gitRepoLink, secureGitUrlPrefix)
+	splitUrl[1] = g.Token + "@" + splitUrl[1]
+	return secureGitUrlPrefix + splitUrl[1]
+}
+
+func (g *GitCredToken) DeepCopyGitCred() GitCred {
+	return g.DeepCopy()
+}
+
+func UnmarshallGitCred(m map[string]interface{}) GitCred {
+	credType := m["type"]
+	delete(m, "type")
+	gitCredBytes, err := json.Marshal(m)
+	if err != nil {
+		panic(err)
+	}
+	switch credType {
+	case serializerutil.GitCredMachineUserType:
+		var gitCredMachineUser GitCredMachineUser
+		_ = json.Unmarshal(gitCredBytes, &gitCredMachineUser)
+		return &gitCredMachineUser
+	case serializerutil.GitCredTokenType:
+		var gitCredToken GitCredToken
+		_ = json.Unmarshal(gitCredBytes, &gitCredToken)
+		return &gitCredToken
+	default:
+		var gitCredOpen GitCredOpen
+		_ = json.Unmarshal(gitCredBytes, &gitCredOpen)
+		return &gitCredOpen
+	}
+}
+
+func UnmarshallGitCredString(str string) GitCred {
+	var m map[string]interface{}
+	_ = json.Unmarshal([]byte(str), &m)
+	return UnmarshallGitCred(m)
+}
+
+func MarshalGitCred(gitCred GitCred) *ordered.OrderedMap {
+	mapObj := ordered.NewOrderedMap()
+
+	switch gitCred.(type) {
+	case *GitCredMachineUser:
+		mapObj.Set("username", gitCred.(*GitCredMachineUser).Username)
+		mapObj.Set("password", gitCred.(*GitCredMachineUser).Password)
+		mapObj.Set("type", serializerutil.GitCredMachineUserType)
+	case *GitCredToken:
+		mapObj.Set("token", gitCred.(*GitCredToken).Token)
+		mapObj.Set("type", serializerutil.GitCredTokenType)
+	default: //Open cred
+		mapObj.Set("type", serializerutil.GitCredOpenType)
+	}
+
+	return mapObj
+}
+
+type GetFileRequest struct {
+	GitRepoSchemaInfo GitRepoSchemaInfo `json:"gitRepoSchemaInfo"`
+	Filename          string            `json:"filename"`
+	GitCommitHash     string            `json:"gitCommitHash"`
 }
